9b0cc60f61ea5f420fdbf3d3a18cfd20b3cc8876
Aubrey Nicoll <aubrey.nicoll@gmail.com>
2021-12-29 08:45:01 +0800

    finish ch 2

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,10 @@
 pub mod first;
+pub mod second;
 
 #[cfg(test)]
 mod tests {
     use super::*;
+
     #[test]
     fn first_list_works() {
         let mut first_list = first::List::new();
@@ -21,5 +23,106 @@ mod tests {
         assert_eq!(Some(101), first_list.pop());
         assert_eq!(Some(1), first_list.pop());
         assert_eq!(None, first_list.pop());
+    }
+
+    #[test]
+    fn second_list_works() {
+        let mut second_list = second::List::new();
+
+        assert_eq!(None, second_list.pop());
+
+        second_list.push(1);
+        second_list.push(2);
+        second_list.push(3);
+
+        assert_eq!(Some(3), second_list.pop());
+        assert_eq!(Some(2), second_list.pop());
+
+        second_list.push(101);
+
+        assert_eq!(Some(101), second_list.pop());
+        assert_eq!(Some(1), second_list.pop());
+        assert_eq!(None, second_list.pop());
+    }
+
+    #[test]
+    fn second_peek_works() {
+        let mut second_list = second::List::new();
+        assert_eq!(None, second_list.peek());
+
+        second_list.push(1);
+        assert_eq!(Some(&1), second_list.peek());
+
+        second_list.push(101);
+        assert_eq!(Some(&101), second_list.peek());
+    }
+
+    #[test]
+    fn second_peek_mut_works() {
+        let mut second_list = second::List::new();
+        assert_eq!(None, second_list.peek_mut());
+
+        second_list.push(1);
+        assert_eq!(Some(&mut 1), second_list.peek_mut());
+
+        second_list.push(101);
+        assert_eq!(Some(&mut 101), second_list.peek_mut());
+
+        if let Some(value) = second_list.peek_mut() {
+            *value = 7;
+        }
+
+        assert_eq!(Some(&mut 7), second_list.peek_mut());
+    }
+
+    #[test]
+    fn second_into_iter_works() {
+        let mut second_list = second::List::new();
+        second_list.push(1);
+        second_list.push(2);
+        second_list.push(3);
+
+        let mut into_iter = second_list.into_iter();
+
+        assert_eq!(Some(3), into_iter.next());
+        assert_eq!(Some(2), into_iter.next());
+        assert_eq!(Some(1), into_iter.next());
+        assert_eq!(None, into_iter.next());
+    }
+
+    #[test]
+    fn second_iter_works() {
+        let mut second_list = second::List::new();
+        second_list.push(1);
+        second_list.push(2);
+        second_list.push(3);
+
+        let mut iter = second_list.iter();
+
+        assert_eq!(Some(&3), iter.next());
+        assert_eq!(Some(&2), iter.next());
+        assert_eq!(Some(&1), iter.next());
+        assert_eq!(None, iter.next());
+    }
+
+    #[test]
+    fn second_iter_mut_works() {
+        let mut second_list = second::List::new();
+        second_list.push(1);
+        second_list.push(2);
+        second_list.push(3);
+
+        let mut iter_mut = second_list.iter_mut();
+
+        while let Some(n) = iter_mut.next() {
+            *n = *n * 2;
+        }
+
+        let mut into_iter = second_list.into_iter();
+
+        assert_eq!(Some(6), into_iter.next());
+        assert_eq!(Some(4), into_iter.next());
+        assert_eq!(Some(2), into_iter.next());
+        assert_eq!(None, into_iter.next());
     }
 }
---
+++ src/second.rs
@@ -0,0 +1,101 @@
+pub struct List<T> {
+    head: Option<Box<Node<T>>>,
+}
+
+struct Node<T> {
+    data: T,
+    next: Option<Box<Node<T>>>,
+}
+
+impl<T> List<T> {
+    pub fn new() -> Self {
+        List { head: None }
+    }
+
+    pub fn push(&mut self, data: T) {
+        let new_node = Node {
+            data,
+            next: self.head.take(),
+        };
+        self.head = Some(Box::new(new_node));
+    }
+
+    pub fn pop(&mut self) -> Option<T> {
+        self.head.take().map(|node| {
+            self.head = node.next;
+            node.data
+        })
+    }
+
+    pub fn peek(&self) -> Option<&T> {
+        self.head.as_ref().map(|node| &node.data)
+    }
+
+    pub fn peek_mut(&mut self) -> Option<&mut T> {
+        self.head.as_mut().map(|node| &mut node.data)
+    }
+}
+
+impl<T> Drop for List<T> {
+    fn drop(&mut self) {
+        let mut current = self.head.take();
+
+        while let Some(mut node) = current {
+            current = node.next.take();
+        }
+    }
+}
+
+pub struct IntoIter<T>(List<T>);
+
+impl<T> List<T> {
+    pub fn into_iter(self) -> IntoIter<T> {
+        IntoIter(self)
+    }
+}
+
+impl<T> Iterator for IntoIter<T> {
+    type Item = T;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        self.0.pop()
+    }
+}
+
+pub struct Iter<'a, T>(Option<&'a Node<T>>);
+
+impl<T> List<T> {
+    pub fn iter(&self) -> Iter<T> {
+        Iter(self.head.as_deref())
+    }
+}
+
+impl<'a, T> Iterator for Iter<'a, T> {
+    type Item = &'a T;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        self.0.map(|node| {
+            self.0 = node.next.as_deref();
+            &node.data
+        })
+    }
+}
+
+pub struct IterMut<'a, T>(Option<&'a mut Node<T>>);
+
+impl<T> List<T> {
+    pub fn iter_mut(&mut self) -> IterMut<T> {
+        IterMut(self.head.as_deref_mut())
+    }
+}
+
+impl<'a, T> Iterator for IterMut<'a, T> {
+    type Item = &'a mut T;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        self.0.take().map(|node| {
+            self.0 = node.next.as_deref_mut();
+            &mut node.data
+        })
+    }
+}
